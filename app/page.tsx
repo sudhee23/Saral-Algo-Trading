492e43d366ee5044dfa11496ec48669ec8c55801
Sathwikiiit <sathwik.sns@gmail.com>
2025-06-18 17:39:00 +0530

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="p-6">
+      <main className="p-6 flex-1">
         <h2 className="text-xl font-medium">Welcome to ALTRA Trading Platform</h2>
       </main>
        <Footer />
