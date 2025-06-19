4fa31b06120d0e40ed436bdc6a2d26cb9460d20f
Sathwikiiit <sathwik.sns@gmail.com>
2025-06-19 14:13:54 +0530

    modified:   components/Graph.tsx

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -11,7 +11,7 @@ import {
 } from "lightweight-charts";
 
 async function getData(ticker:string): Promise<CandlestickData<Time>[]> {
-  const res = await fetch("/ohlcv.json");
+  const res = await fetch("/Saral-Algo-Trading/ohlcv.json");
   const json = await res.json();
   return json[ticker]; // Or whichever ticker
 }
